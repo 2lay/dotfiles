dfa00ac35d53d5ffaa3e64db1a7b9eb6a7393f96
2lay <mail@2lay.net>
2025-01-24 21:31:50 +0100

    feat: fixes & makefile

--- flake.nix
+++ flake.nix
@@ -1,26 +1,23 @@
 {
-    description = "Ellie's Home Manager config";
-
-    inputs =  {
-        nixpgs.url = "nixpks/nixos-24.11-pre";
-
-        home-manager = {
-            url = "github.com:nix-community/home-manager/release-24.11-pre";
-            inputs.nixpgs.follows = "nixpkgs";
-        };
-    };
-
-    outputs = { nixpkgs, home-manager, ... }:
-        let
-            lib = nixpkgs.lib;
-            system = "x86_64-linux";
-            pkgs = import nikpkgs { inherit system; };
-        in {
-            homeConfigurations = {
-                profile = home-manager.lib.homeManagerConfiguration {
-                    inherit pkgs;
-                    modules = [ ./home.nix ];
-                };
-            };
-        };
+   description = "Ellie's Home Manager config";
+   inputs =  {
+       nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+       home-manager = {
+           url = "github:nix-community/home-manager";
+           inputs.nixpkgs.follows = "nixpkgs";
+       };
+   };
+   outputs = { nixpkgs, home-manager, ... }:
+       let
+           lib = nixpkgs.lib;
+           system = "x86_64-linux";
+           pkgs = import nixpkgs { inherit system; };
+       in {
+           homeConfigurations = {
+               profile = home-manager.lib.homeManagerConfiguration {
+                   inherit pkgs;
+                   modules = [ ./home.nix ];
+               };
+           };
+       };
 }
--- home.nix
+++ home.nix
@@ -1,12 +1,12 @@
-{ lib, pgks ... }:
+{ lib, pkgs, ... }:
 {
     home = {
         packages = with pkgs; [
             hello
         ];
 
-        username = "ellie"
-        homeDirectory = "/home/ellie"
+        username = "ellie";
+        homeDirectory = "/home/ellie";
 
         # IMPORTANT: Do not change this after the first build
         stateVersion = "23.11";
